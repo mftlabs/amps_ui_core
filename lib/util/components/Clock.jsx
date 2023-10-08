949c7996d1c90c075d7bfdc641b46f0a1b0510cc
aram0112 <48899851+aram0112@users.noreply.github.com>
2023-10-07 20:24:47 -0500

    Update Clock.jsx

--- lib/util/components/Clock.jsx
+++ lib/util/components/Clock.jsx
@@ -4,33 +4,31 @@ import { Box, Typography } from "@mui/material";
 
 import moment from "moment-timezone";
 
-export const Clock = ({ timezone }) => {
+export const Clock = ({ timezone, ...other }) => {
   const [dt, setDt] = useState(
     new Date().toLocaleString("en-US", { timeZone: timezone })
   );
 
   useEffect(() => {
     let secTimer = setInterval(() => {
-      setDt(new Date().toLocaleTimeString("en-US", { timeZone: timezone }));
+      setDt(new Date().toLocaleString("en-US", { timeZone: timezone }));
     }, 1000);
 
     return () => clearInterval(secTimer);
   }, []);
 
   return (
-    <>
-      <Typography noWrap={true} color="neutral.500">
-        <Box
-          component="span"
-          sx={{
-            display: {
-              xs: "none",
-              lg: "unset",
-            },
-          }}
-        ></Box>
-        {dt} {moment().tz(timezone).zoneAbbr()}
-      </Typography>
-    </>
+    <Typography {...other} noWrap={true} color="neutral.500">
+      <Box
+        component="span"
+        sx={{
+          display: {
+            xs: "none",
+            lg: "unset",
+          },
+        }}
+      ></Box>
+      {dt} {moment().tz(timezone).zoneAbbr()}
+    </Typography>
   );
 };
